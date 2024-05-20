ab9127b572381536e7f3bbf8d95f47639b8ebb62
PedroR-19 <p.scianni@aluno.ifsp.edu.br>
2024-05-20 16:58:43 -0300

    user_type ok

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 User = get_user_model()
 
-
 class Profile(models.Model):
-    profile = models.OneToOneField(User, on_delete=models.CASCADE)
+    USER_TYPE_CHOICES = [
+        ('ONG', 'ONG'),
+        ('Voluntier', 'Voluntier'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default='', blank=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, blank=True)
